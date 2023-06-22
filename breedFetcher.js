dee7abdb950ec1436a1d1fb11d6ffc22defdd970
Sunny Hundal <amandip.h@gmail.com>
2023-06-21 22:26:22 -0700

    Eslint code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,24 @@
 const request = require("request");
-const breed = process.argv[2];
 
-const fetchBreedDescription = function (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   let description;
-  let errorDescription;
   request(
     `http://api.thecatapi.com/v1/breeds/search?q=` + breedName,
-    function (error, response, body) {
+    function(error, response, body) {
       let apiRequest = JSON.parse(body);
 
       if (error) {
         return callback(error, null);
       }
       
-      if (apiRequest.length === 0 ) {
+      if (apiRequest.length === 0) {
         return callback("no data sent back", null);
       }
       
       if (JSON.parse(body).length > 0) {
         description = apiRequest[0].description;
         return callback(null, description);
-      } 
+      }
     
     }
   );
